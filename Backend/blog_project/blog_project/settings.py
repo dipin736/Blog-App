c402dcf1c0a5f94399cc8fd9eceb9a6728e96050
Dipin Karunakaran <114206930+dipin736@users.noreply.github.com>
2024-10-08 00:41:13 +0530

    settings.py

--- Backend/blog_project/blog_project/settings.py
+++ Backend/blog_project/blog_project/settings.py
@@ -141,11 +141,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory for collectstatic
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Directory for static files used in development
 ]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
